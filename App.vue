1d7543d9d4c34f1fba395f07d40588bf01fe0fe2
saman-zahra <122096807+saman-zahra@users.noreply.github.com>
2026-01-07 10:07:27 +0500

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <ImageConverter />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import ImageConverter from './components/ImageConverter.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    ImageConverter
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
